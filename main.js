d6269f0b6b9496765d119034464979d2a606020e
Lukas Bures <burylukas@seznam.cz>
2017-07-13 21:06:31 +0200

    added login-screen

--- main.js
+++ main.js
@@ -97,7 +97,7 @@ function createWindow() {
 
     // and load the index.html of the app.
     win.loadURL(url.format({
-        pathname: path.join(__dirname, "index.html"),
+        pathname: path.join(__dirname, "login.html"),
         protocol: "file:",
         slashes: true
     }));
