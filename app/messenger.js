a8286f4f33ca0eb92d42b78d4e6c224e7e06f804
Petr Neduchal <neduchal@gmail.com>
2017-08-21 17:37:52 +0200

    Design update
    - button design
    - messenger AddContactDialog design

--- app/messenger.js
+++ app/messenger.js
@@ -9,3 +9,31 @@ function setMessageValueIfEmpty() {
         document.getElementById("sendToAddress").value = "Message";
     }
 }
+
+function addContactDialog() {
+    document.getElementById("darkContainer").style.transition = "0.5s";
+    document.getElementById("darkContainer").style.zIndex = "1";
+    document.getElementById("darkContainer").style.opacity = "0.7";
+    document.getElementById("addContactDialog").style.zIndex = "2";
+    document.getElementById("addContactDialog").style.opacity = "1";
+}
+
+function saveContact() {
+    document.getElementById("darkContainer").style.transition = "0s";
+    document.getElementById("darkContainer").style.opacity = "0";
+    document.getElementById("darkContainer").style.zIndex = "-1";
+    document.getElementById("addContactDialog").style.zIndex = "-1";
+    document.getElementById("addContactDialog").style.opacity = "0";
+
+
+}
+
+function closeDialog() {
+    document.getElementById("darkContainer").style.transition = "0s";
+    document.getElementById("darkContainer").style.opacity = "0";
+    document.getElementById("darkContainer").style.zIndex = "-1";
+    document.getElementById("addContactDialog").style.zIndex = "-1";
+    document.getElementById("addContactDialog").style.opacity = "0";
+
+
+}
