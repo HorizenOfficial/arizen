2e9534477580dea81bee5918415dccb4e392ede7
Lukáš Bureš <burylukas@seznam.cz>
2017-09-21 09:59:27 +0200

    About dialog updated

--- app/common.js
+++ app/common.js
@@ -48,7 +48,7 @@ function aboutDialog() {
     document.getElementById("sidenavIMG").style.transition = "0s";
     document.getElementById("sidenavIMG").style.opacity = "0";
     document.getElementById("aboutContent").innerHTML = "Arizen version: " + pckg.version + "\<br\>";
-    document.getElementById("aboutContent").innerHTML += "Authors: " + pckg.author + "\<br\>";
+    document.getElementById("aboutContent").innerHTML += "Authors: " + pckg.contributors.name + "," + pckg.contributors.email + "\<br\>";
     document.getElementById("aboutContent").innerHTML += "License: " + pckg.license;
     document.getElementById("darkContainer").style.transition = "0.5s";
     document.getElementById("darkContainer").style.zIndex = "1";
