26140395eba1fa5d61c759f9157d820e956857bd
gsfakianakis <g.e.sfakianakis@gmail.com>
2018-10-09 10:28:04 +0200

    Test commit 2.

--- app/main.js
+++ app/main.js
@@ -466,8 +466,8 @@ function setSettings(newSettings) {
         axiosApi = axios.create({
             baseURL: settings.domainFrontingUrl,
             headers: {
-                "Host": settings.domainFrontingHost,
-                "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:61.0) Gecko/20100101 Firefox/61.0"
+                "Host": settings.domainFrontingHost//,
+                //"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:61.0) Gecko/20100101 Firefox/61.0"
 
             },
             timeout: 30000,
