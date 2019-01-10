a253ad6148a0ea5ef03629a47e8e7a1a3695e773
Nathan King <nathanking3590@gmail.com>
2019-01-10 16:58:25 +0000

    Add build script

--- ci/script.sh
+++ ci/script.sh
@@ -4,4 +4,16 @@
 
 set -e
 
-npm run dist
+#npm run dist
+
+#apt install -y wine
+
+if [[ ${PLATFORM} == "osx" ]]; then
+    npx electron-builder build -m
+elif [[ ${PLATFORM} == "windows" ]]; then
+    npx electron-builder build -w
+elif [[ ${PLATFORM} == "linux" ]]; then
+    npx electron-builder build -l
+else
+    echo "Unknown OS"
+fi
