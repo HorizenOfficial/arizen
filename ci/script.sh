409ddb82b9475e4f61e2fa0e72748013cb845c09
cronicc <cronic@zensystem.io>
2019-02-27 17:40:51 +0000

    Filter out LF/CRLF in env varibles causing problems with docker

--- ci/script.sh
+++ ci/script.sh
@@ -13,7 +13,7 @@ fi
 # Continue even on failing yarn audit, sometimes vulnerablilities cannot be fixed yet, but at least we have a log of them.
 if [ "${TRAVIS_OS_NAME}" == "linux" ]; then
   docker run --rm \
-    --env-file <(env | grep -iE 'DEBUG|NODE_|ELECTRON_|YARN_|NPM_|CI|CIRCLE|TRAVIS|APPVEYOR_|CSC_|_TOKEN|_KEY|AWS_|STRIP|BUILD_|TZ') \
+    --env-file <(env | grep -vE '\r|\n' | grep -iE 'DEBUG|NODE_|ELECTRON_|YARN_|NPM_|CI|CIRCLE|TRAVIS|APPVEYOR_|CSC_|_TOKEN|_KEY|AWS_|STRIP|BUILD_|TZ') \
     -v "${PWD}":/project \
     -v ~/.cache/electron:/root/.cache/electron \
     -v ~/.cache/electron-builder:/root/.cache/electron-builder \
